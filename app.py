c406f28ef973ee3e48163854a81cdcafc1cba9b1
MaksimKhmylov <maksimhmylov97@gmail.com>
2024-11-03 12:25:55 +0300

    second

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import datetime as date
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     age = date.datetime.now().year-2010 if date.datetime.now().month >= 10 else date.datetime.now().year-2010-1
