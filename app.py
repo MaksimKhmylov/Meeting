91085cbcf2d7920b7a057095b1dab5b681614b69
MaksimKhmylov <maksimhmylov97@gmail.com>
2024-11-03 12:24:34 +0300

    first

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-import
+import datetime as date
 app = Flask(__name__)
 @app.route('/')
 def index():
-    context =
-    return render_template('index.html')
+    age = date.datetime.now().year-2010 if date.datetime.now().month >= 10 else date.datetime.now().year-2010-1
+    return render_template('index.html', age=age)
 
 if __name__ == '__main__':
     app.run(debug=True)
